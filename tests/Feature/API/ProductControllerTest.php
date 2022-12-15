05d951467a27edce3a25aa9bb68d2a41bfdbd3ea
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-12-15 17:48:26 -0300

    Testes de Software Laravel 358

--- tests/Feature/API/ProductControllerTest.php
+++ tests/Feature/API/ProductControllerTest.php
@@ -2,22 +2,33 @@
 
 namespace Tests\Feature\API;
 
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class ProductControllerTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_should_product_get_endpoints_list_all_products()
     {
+        Product::factory(3)
+            ->create();
+
         $response = $this->getJson('/api/products');
 
         $response->assertStatus(200);
+
+        $response->assertJson(function (AssertableJson $json) {
+            $json->count('data', 3)
+                ->etc();
+        });
     }
 }
